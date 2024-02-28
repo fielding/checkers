827dcc837e7801ba1e12b033a9a889af864b5cc8
Fielding Johnston <fielding@justfielding.com>
2024-02-28 16:06:55 -0600

    put error types in proper place

--- x/checkers/types/full_game.go
+++ x/checkers/types/full_game.go
@@ -44,9 +44,3 @@ func (storedGame StoredGame) Validate() (err error) {
 	_, err = storedGame.ParseGame()
 	return err
 }
-
-var (
-	ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1100, "black address is invalid: %s")
-	ErrInvalidRed       = sdkerrors.Register(ModuleName, 1101, "red address is invalid: %s")
-	ErrGameNotParseable = sdkerrors.Register(ModuleName, 1102, "game cannot be parsed")
-)
